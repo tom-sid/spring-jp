e20240c361ac0fd07457c683416ef9af1f81713b
Reji <Reji@DESKTOP-5T8LV75>
2017-08-22 09:06:03 -0500

    Using dependency injection

--- demo-1/src/main/java/com/example/demo1/Demo1Application.java
+++ demo-1/src/main/java/com/example/demo1/Demo1Application.java
@@ -2,13 +2,14 @@ package com.example.demo1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Demo1Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Demo1Application.class, args);
-		Person bean = new Person();
+		ConfigurableApplicationContext context = SpringApplication.run(Demo1Application.class, args);
+		Person bean = context.getBean(Person.class);
 		bean.speak();
 	}
 }
--- demo-1/src/main/java/com/example/demo1/Person.java
+++ demo-1/src/main/java/com/example/demo1/Person.java
@@ -1,5 +1,8 @@
 package com.example.demo1;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Person {
 	public void speak() {
 		System.out.println("Hello World!");
